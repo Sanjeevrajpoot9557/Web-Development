431e6e0d529cde501855c55a01b9eee2a8cc9eae
Sanjeev Rajpoot <sanjeevrajpootlmp9696@gmail.com>
2022-09-03 09:59:51 -0400

    I have added all files of the web development folder in the github

---
+++ if els13.py
@@ -0,0 +1,11 @@
+"""var1=5
+var2=10
+var3=int(input())
+if var2>var1:
+   print("greater")
+else:
+  print("lesser")	"""
+  list=[2,3,4,5]
+  print(5 in list)
+  if 5 in list:
+   print ("yes its the list")
